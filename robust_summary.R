bd2454974f0a96e307acc3639dd59b76beb0f81e
IsidoreBeautrelet <dibiasia@tcd.ie>
2016-08-20 10:22:36 +0200

    include clustered standard errors

--- robust_summary.R
+++ robust_summary.R
@@ -1,6 +1,7 @@
 summary.lm <- function (object, correlation = FALSE, 
-symbolic.cor = FALSE, robust=FALSE,...) {
-# add extension for robust standard errors
+                        symbolic.cor = FALSE, robust=FALSE,
+                        cluster=c(NULL,NULL),...) {
+  # add extension for robust standard errors
   if(robust==TRUE){ 
     # save variable that are necessary to calcualte robust sd
     X <- model.matrix(object)
@@ -26,7 +27,50 @@ symbolic.cor = FALSE, robust=FALSE,...) {
     # square roots of the diagonal elements
     rstdh <- dfc_r*sqrt(diag(varcovar))
   }
-  
+  # add extension for clustered standard errors
+  if(!is.null(cluster)&robust==T){warning("Robust standard errors are calculated. Set robust=F to calculate clustered standard errors.")}
+  if(!is.null(cluster)&robust==F){
+    n_coef <- all.vars(object$call)[-length(all.vars(object$call))]
+    dat <- na.omit(get(all.vars(object$call)[length(all.vars(object$call))])[,c(n_coef,cluster)])
+    if(nrow(dat)<nrow(object$model)){stop("Not all observation have a cluster.")}
+    if(length(cluster)==""){stop("No variable for clustering provided.")}
+    if(length(cluster)>2){stop("The function only allows max. 2 clusters. You provided more.")}
+    
+    if(length(cluster)==1){
+      cluster <- dat[,cluster]
+    require(sandwich, quietly = TRUE)
+    M <- length(unique(cluster))
+    N <- length(cluster)
+    K <- object$rank
+    dfc <- (M/(M-1))*((N-1)/(N-K))
+    uj  <- na.omit(apply(estfun(object),2, function(x) tapply(x, cluster, sum)));
+    vcovCL <- dfc*sandwich(object, meat=crossprod(uj)/N)
+    rstdh <- sqrt(diag(vcovCL))
+    } 
+    if(length(cluster)==2){
+      library(sandwich,quietly = TRUE)
+      cluster1 <- dat[,cluster[1]]
+      cluster2 <- dat[,cluster[2]]
+      cluster12 = paste(cluster1,cluster2, sep="")
+      M1  <- length(unique(cluster1))
+      M2  <- length(unique(cluster2))   
+      M12 <- length(unique(cluster12))
+      N   <- length(cluster1)          
+      K   <- fm$rank             
+      dfc1  <- (M1/(M1-1))*((N-1)/(N-K))  
+      dfc2  <- (M2/(M2-1))*((N-1)/(N-K))  
+      dfc12 <- (M12/(M12-1))*((N-1)/(N-K))  
+      u1j   <- apply(estfun(fm), 2, function(x) tapply(x, cluster1,  sum)) 
+      u2j   <- apply(estfun(fm), 2, function(x) tapply(x, cluster2,  sum)) 
+      u12j  <- apply(estfun(fm), 2, function(x) tapply(x, cluster12, sum)) 
+      vc1   <-  dfc1*sandwich(fm, meat=crossprod(u1j)/N )
+      vc2   <-  dfc2*sandwich(fm, meat=crossprod(u2j)/N )
+      vc12  <- dfc12*sandwich(fm, meat=crossprod(u12j)/N)
+      vcovMCL <- vc1 + vc2 - vc12
+      rstdh <- sqrt(diag(vcovMCL))
+    } 
+    
+  }
   z <- object
   p <- z$rank
   rdf <- z$df.residual
@@ -87,7 +131,9 @@ symbolic.cor = FALSE, robust=FALSE,...) {
   p1 <- 1L:p
   R <- chol2inv(Qr$qr[p1, p1, drop = FALSE])
   se <- sqrt(diag(R) * resvar)
+  
   if(robust==T){se <- rstdh}
+  if(!is.null(cluster)&robust==F){se <- rstdh}
   est <- z$coefficients[Qr$pivot[p1]]
   tval <- est/se
   ans <- z[c("call", "terms", if (!is.null(z$weights)) "weights")]
